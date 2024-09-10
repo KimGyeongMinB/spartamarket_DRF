0dbdd4dcdad752e950b071e5ef6ddacb4ecbd17a
unknown <pigking41@gmail.com>
2024-09-10 11:49:49 +0900

    feature 상품 수정 및 삭제
    상품 수정 기능 추가
    - serializer 사용
    상품 삭제 기능 추가
    - serializer 사용

--- products/views.py
+++ products/views.py
@@ -33,3 +33,28 @@ class GoodsAPIView(APIView):
                         "전체 페이지" :  paginator.num_pages,
                         "현재 페이지" : goods.number,
                         "목록":serializer.data})
+    
+    # 상품 수정
+    def put(self, request, productId):
+        if request.user.is_authenticated: # 로그인 상태일때
+            goods_put = Goods.objects.get(id=productId)
+            serializer = GoodsSerializer(goods_put, data=request.data, partial=True)
+
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+            return Response({"message":"상품 수정이 불가능한 글입니다."}, status=400)
+        return Response({"message":"이 기능은 로그인 해야만 이용이 가능합니다."},status=400)
+    
+
+# 상품 삭제
+    def delete(self, request, productId):
+        if request.user.is_authenticated: # 로그인 상태일때
+            goods_del = get_object_or_404(Goods, id=productId)
+
+            if request.user == goods_del.user:
+                goods_del.delete()
+                return Response("삭제되었습니다", status=200)
+            else:
+                return Response({"message":"글 작성자만이 삭제할 수 있습니다"}, status=403)
+        return Response({"message":"이 기능은 로그인 해야만 이용이 가능합니다."},status=400)
