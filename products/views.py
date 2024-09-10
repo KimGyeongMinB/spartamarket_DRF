e22d3ccde2e2b11797036e1c94c69ca382b840e8
unknown <pigking41@gmail.com>
2024-09-10 11:48:50 +0900

    feature 상품 목록 조회 기능
    상품목록 조회 기능
    페이지네이션 적용 완료

--- products/views.py
+++ products/views.py
@@ -20,3 +20,16 @@ class GoodsAPIView(APIView):
                 return Response({"message":"상품 등록이 완료되었습니다"}, status=200)
             return Response({"message":"상품등록을 실패하였습니다", "errors": serializer.errors}, status=400)
         return Response({"message":"이 기능은 로그인을 해야만 이용할 수 있는 기능입니다."}, status=400)
+    
+
+    # 상품 목록조회
+    def get(self, request):
+        page = request.GET.get('page','1') # 페이지
+        goods_list = Goods.objects.all()
+        paginator = Paginator(goods_list, 10)
+        goods = paginator.get_page(page)
+        serializer = GoodsSerializer(goods.object_list, many=True)
+        return Response({"전체상품 수" : paginator.count,
+                        "전체 페이지" :  paginator.num_pages,
+                        "현재 페이지" : goods.number,
+                        "목록":serializer.data})
